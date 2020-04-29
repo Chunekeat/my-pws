8ef4bea26ab787b8bcee72031aa5a45eaabaccfa
Chunekeat <chunekeat@yahoo.com>
2020-04-29 04:19:08 +0000

    remove git.ignore
    remove gitignore

---
+++ config.py
@@ -0,0 +1,6 @@
+ecs_test_drive = {
+    'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
+    'ecs_access_key_id' : '132323677695057493@ecstestdrive.emc.com',
+    'ecs_secret_key' : '5cOVOF04M1NzKvzBYbB4ssO/V30BE5KPxXcZbm2n',
+    'ecs_bucket_name' : 'photo-album'
+}
